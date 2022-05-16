3f0f523364eeac439cb0c67ef4d0ecd57be4da81
Vinicius Santos <vinicius.santos@inmetrics.com.br>
2022-05-16 08:27:21 -0300

    Fim aula 2 - responsabilidade unica

---
+++ src/Alura.LeilaoOnline.WebApp/Controllers/LeilaoApiController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Alura.LeilaoOnline.WebApp.Models;
+using Alura.LeilaoOnline.WebApp.Dados;
+
+namespace Alura.LeilaoOnline.WebApp.Controllers
+{
+    [ApiController]
+    [Route("/api/leiloes")]
+    public class LeilaoApiController : ControllerBase
+    {
+        LeilaoDao _dao;
+
+        public LeilaoApiController()
+        {
+            _dao = new LeilaoDao();
+        }
+
+        [HttpGet]
+        public IActionResult EndpointGetLeiloes()
+        {
+            var leiloes = _dao.BuscarLeiloes();
+            return Ok(leiloes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult EndpointGetLeilaoById(int id)
+        {
+            var leilao = _dao.BuscarPorId(id);
+            if (leilao == null)
+            {
+                return NotFound();
+            }
+            return Ok(leilao);
+        }
+
+        [HttpPost]
+        public IActionResult EndpointPostLeilao(Leilao leilao)
+        {
+            _dao.Inserir(leilao);
+            return Ok(leilao);
+        }
+
+        [HttpPut]
+        public IActionResult EndpointPutLeilao(Leilao leilao)
+        {
+            _dao.Alterar(leilao);
+            return Ok(leilao);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult EndpointDeleteLeilao(int id)
+        {
+            var leilao = _dao.BuscarPorId(id);
+            if (leilao == null)
+            {
+                return NotFound();
+            }
+            _dao.Excluir(leilao);
+            return NoContent();
+        }
+    }
+}
