8fcade8e67da5690c6ccdaa8de02355d6e4f6cb8
Vinicius Santos <vinicius.santos@inmetrics.com.br>
2022-05-19 08:10:48 -0300

    Fim 5 aula - segregacao de interfaces

--- src/Alura.LeilaoOnline.WebApp/Dados/ICategoriaDao.cs
+++ src/Alura.LeilaoOnline.WebApp/Dados/ICategoriaDao.cs
@@ -1,11 +1,8 @@
 ﻿using Alura.LeilaoOnline.WebApp.Models;
-using System.Collections.Generic;
 
 namespace Alura.LeilaoOnline.WebApp.Dados
 {
-    public interface ICategoriaDao
+    public interface ICategoriaDao: IQuery<Categoria>
     {
-        IEnumerable<Categoria> ConsultaCategorias();
-        Categoria ConsultaCategoriaPorId(int id);
     }
 }
---
+++ src/Alura.LeilaoOnline.WebApp/Dados/ICommand.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Alura.LeilaoOnline.WebApp.Dados
+{
+    public interface ICommand<T>
+    {
+        void Incluir(T entity);
+        void Alterar(T entity);
+        void Excluir(T entity);
+    }
+}
